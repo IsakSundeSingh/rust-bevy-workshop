0d778a956f744a1ffd2d7b154321f73a9831aa6f
Isak Sunde Singh <isak.sunde.singh@gmail.com>
2024-12-05 22:11:21 +0100

    Add bevy game setup and how to display images in Bevy

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,18 @@
 use bevy::prelude::*;
 
 fn main() {
-    App::new().add_plugins(DefaultPlugins).run();
+    App::new()
+        .add_plugins(DefaultPlugins)
+        .add_systems(Startup, (spawn_camera, spawn_player))
+        .run();
+}
+
+fn spawn_camera(mut commands: Commands) {
+    commands.spawn(Camera2d);
+}
+
+fn spawn_santa(mut commands: Commands, asset_server: Res<AssetServer>) {
+    commands
+        .spawn_empty()
+        .insert(Sprite::from_image(asset_server.load("santa.png")));
 }
