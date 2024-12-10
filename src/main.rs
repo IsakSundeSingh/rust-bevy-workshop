1e481b223cc40b1b00dd9bc0ad92af9917fb311a
Isak Sunde Singh <isak.sunde.singh@gmail.com>
2024-12-10 17:04:52 +0100

    Assignment 4: move the elf

--- src/main.rs
+++ src/main.rs
@@ -24,7 +24,7 @@ fn main() {
         .init_state::<DebugMode>()
         .add_plugins(WorldInspectorPlugin::new().run_if(in_state(DebugMode::On)))
         .add_systems(Startup, (spawn_camera, spawn_santa, spawn_elf))
-        .add_systems(FixedUpdate, (toggle_debug_mode, move_santa))
+        .add_systems(FixedUpdate, (toggle_debug_mode, move_santa, move_elf))
         .run();
 }
 
@@ -62,12 +62,42 @@ fn move_santa(mut query: Query<&mut Transform, With<Santa>>, time: Res<Time>) {
     santa_transform.translation = circle.sample(time.elapsed_secs_wrapped()).unwrap() * 250.0;
 }
 
+fn move_elf(
+    mut query: Query<&mut Transform, With<Elf>>,
+    keyboard_input: Res<ButtonInput<KeyCode>>,
+) {
+    let mut elf_transform = query.single_mut();
+
+    let move_left = keyboard_input.any_pressed([KeyCode::ArrowLeft, KeyCode::KeyA]);
+    let move_right = keyboard_input.any_pressed([KeyCode::ArrowRight, KeyCode::KeyD]);
+    let move_up = keyboard_input.any_pressed([KeyCode::ArrowUp, KeyCode::KeyW]);
+    let move_down = keyboard_input.any_pressed([KeyCode::ArrowDown, KeyCode::KeyS]);
+
+    let mut delta = Vec2::ZERO;
+    if move_left {
+        delta.x -= 1.0;
+    }
+    if move_right {
+        delta.x += 1.0;
+    }
+    if move_down {
+        delta.y -= 1.0;
+    }
+    if move_up {
+        delta.y += 1.0;
+    }
+    let delta = delta.normalize_or_zero();
+
+    let z = elf_transform.translation.z;
+    elf_transform.translation += Vec3::new(delta.x, delta.y, z) * 10.0;
+}
+
 fn toggle_debug_mode(
     keyboard_input: Res<ButtonInput<KeyCode>>,
     debug_mode: Res<State<DebugMode>>,
     mut next_state: ResMut<NextState<DebugMode>>,
 ) {
-    if keyboard_input.just_pressed(KeyCode::KeyD) {
+    if keyboard_input.just_pressed(KeyCode::KeyD) && keyboard_input.pressed(KeyCode::ControlLeft) {
         next_state.set(debug_mode.get().toggle());
     }
 }
